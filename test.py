78bc148761d2acebea23355a240ec76332831da1
avengers400 <75693165+avengers400@users.noreply.github.com>
2022-07-22 20:09:11 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+d = {}
+num = int(input("Enter the dictionaries: "))
+for i in range(num):
+     dict_x = input("enter parent dict: ")
+     d[dict_x] = {}
+     firstkey = input("enter first key : ")
+     d[dict_x][firstkey] = {}
+     secondkey = input("enter 2nd key: ")
+     value = input("enter the value: ")
+     d[dict_x][firstkey][secondkey] = value
+#
+#
+# print(d)
+def dict(a):
+      for id in a:
+
+          for k in a[id]:
+
+              for l in a[id][k]:
+                  print(a[id][k][l])
+
+result = dict(d)
